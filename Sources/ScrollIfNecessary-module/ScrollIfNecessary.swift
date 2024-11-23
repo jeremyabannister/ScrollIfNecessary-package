34d42b188f40c2e04c00f7bfa8586be88b4cfbd3
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-11-23 23:50:38 +0100

    build: upgrade to Swift 6.0

--- Sources/ScrollIfNecessary-module/ScrollIfNecessary.swift
+++ Sources/ScrollIfNecessary-module/ScrollIfNecessary.swift
@@ -5,7 +5,6 @@
 //  Created by Jeremy Bannister on 4/28/23.
 //
 
-///
 @available(iOS 16.0, macOS 13.0, watchOS 9, tvOS 16, *)
 public func ScrollIfNecessary(
     along axes: Axis.Set,
@@ -13,13 +12,9 @@ public func ScrollIfNecessary(
     content: @escaping ()->some View
 ) -> some View {
     
-    ///
     ViewThatFits {
-        
-        ///
         content()
         
-        ///
         ScrollView(axes, showsIndicators: showsIndicators) {
             content()
                 .padding(2)
--- Tests/ScrollIfNecessary-module-tests/_exported_imports.swift
+++ Tests/ScrollIfNecessary-module-tests/_exported_imports.swift
@@ -5,5 +5,4 @@
 //  Created by Jeremy Bannister on 4/28/23.
 //
 
-///
 @_exported import ScrollIfNecessary_module
